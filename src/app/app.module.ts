b6ca9b6606c7120e273a58d97882f5dc0a79988e
jswang0037 <jswang0037@gmail.com>
2024-10-30 20:24:43 +0800

    feat: 🎸 init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"little-games-a78c1","appId":"1:604049550614:web:2e911b7ea7b0ed57bef437","storageBucket":"little-games-a78c1.appspot.com","apiKey":"AIzaSyBPXWdWdIWOonMLe5-G4nvtecf_sIfDwl8","authDomain":"little-games-a78c1.firebaseapp.com","messagingSenderId":"604049550614","measurementId":"G-7TGKKYXF3T"})),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
